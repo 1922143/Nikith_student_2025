fec9c0b9445443fa013b7c36a1dae88999fb2608
1922143 <nikithmuralikrishnan@gmail.com>
2024-10-06 23:27:01 -0700

    hw

---
+++ _notebooks/Foundation/2024-10-06-3.1 Javascript Homework.js
@@ -0,0 +1,33 @@
+let myName = "Nikith Muralikrishnan";
+
+let myAge = 16;
+
+let pi = 3.14159;
+
+console.log(myName);
+console.log(myAge);
+console.log(pi);
+
+
+
+favorite_food = 'pizza' //snake_case
+FavoriteHobby = 'videogames' //PascalCase
+favoriteColor = 'red' //camelCase
+
+console.log(favorite_food);
+console.log(FavoriteHobby);
+console.log(favoriteColor);
+
+
+
+let about_me = ["Nikith", "16", "2.79"]; //array (list of elements)
+
+console.log(about_me);
+
+let me = { //dictionary
+    name: "Nikith",
+    age: 16,
+    favorite_number: 2.79,
+};
+
+console.log(me);
