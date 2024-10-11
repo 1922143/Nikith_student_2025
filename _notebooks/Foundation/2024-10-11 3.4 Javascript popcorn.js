e7a4701f629e556325686c2fb5d5df7891dd0b5c
1922143 <nikithmuralikrishnan@gmail.com>
2024-10-11 07:18:44 -0700

    js 3.4 hw

---
+++ _notebooks/Foundation/2024-10-11 3.4 Javascript popcorn.js
@@ -0,0 +1,16 @@
+
+let last_name = "Muralikrishnan"; 
+let length = last_name.length;
+
+console.log(length);  //Output: 14
+
+
+let first_name = "Nikith";
+let full_name = first_name + " " + last_name;
+
+console.log(full_name); //Output: Nikith Muralikrishnan
+
+
+let substring = full_name.substring(1, 8);
+
+console.log(substring); //Output: ikith M
